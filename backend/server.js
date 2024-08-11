a7d7d5b484d44f5877c91e1c036ed96ba52f3d48
ANIMESHSINGH01 <animeshsingh624@gmail.com>
2024-08-11 22:50:35 +0530

    adding backend folder

---
+++ backend/server.js
@@ -0,0 +1,55 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import passport from "passport";
+import session from "express-session";
+import path from "path";
+
+import "./passport/github.auth.js";
+import userRoutes from "./routes/user.route.js";
+import exploreRoutes from "./routes/explore.route.js";
+import authRoutes from "./routes/authroute.js";
+
+import connectMongoDB from "./db/connectMongoDB.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
+
+console.log("dirname", __dirname);
+
+app.use(
+  session({ secret: "keyboard cat", resave: false, saveUninitialized: false })
+);
+// Initialize Passport!  Also use passport.session() middleware, to support
+// persistent login sessions (recommended).
+app.use(passport.initialize());
+app.use(passport.session());
+
+// app.use(cors({
+//   origin: "http://localhost:5000", // Allow requests from this origin
+//   credentials: true, // Allow cookies to be sent with the request
+// }));
+const corsOptions = {
+  origin: "*",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
+
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/explore", exploreRoutes);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
+app.listen(PORT, () => {
+  console.log(`Server started on http://localhost:${PORT}`);
+  connectMongoDB();
+});
